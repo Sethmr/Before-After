e97ec79fc3bdb631df245b6c8c0fcc4255c66ed1
Sethmr <seth.rininger@students.fhu.edu>
2015-10-22 23:12:59 -0500

    basic profile cosmetics added

---
+++ BeforeAndAfter/Stuff.swift
@@ -0,0 +1,13 @@
+//
+//  Stuff.swift
+//  BeforeAndAfter
+//
+//  Created by Seth Rininger on 10/22/15.
+//  Copyright © 2015 Seth Rininger. All rights reserved.
+//
+
+import UIKit
+
+class Stuff: NSObject {
+
+}
